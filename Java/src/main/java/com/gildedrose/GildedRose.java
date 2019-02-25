538f43fdc47775847728d26d396154578c3afc5e
jaredgreen <jgreen2@pillartechnology.com>
2019-02-25 16:42:44 -0600

    Added unit tests and extracted key constants from GildedRose

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,12 @@
 package com.gildedrose;
 
 class GildedRose {
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final int MAXIMUM_QUALITY = 50;
+    public static final int FAR_OUT_BACKSTAGE_PASS_SELLIN = 11;
+    public static final int CLOSE_BACKSTAGE_PASS_SELLIN = 6;
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -8,52 +14,56 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+        for (Item item : items) {
+            String itemName = item.name;
+            boolean isSulfuras = itemName.equals(SULFURAS);
+            boolean isBackstagePass = itemName.equals(BACKSTAGE_PASSES);
+            boolean isAgedBrie = itemName.equals(AGED_BRIE);
+
+            if (!isAgedBrie && !isBackstagePass) {
+                if (item.quality > 0) {
+                    if (!isSulfuras) {
+                        item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < MAXIMUM_QUALITY) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (isBackstagePass) {
+                        if (item.sellIn < FAR_OUT_BACKSTAGE_PASS_SELLIN) {
+                            if (item.quality < MAXIMUM_QUALITY) {
+                                item.quality = item.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (item.sellIn < CLOSE_BACKSTAGE_PASS_SELLIN) {
+                            if (item.quality < MAXIMUM_QUALITY) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!isSulfuras) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!isAgedBrie) {
+                    if (!isBackstagePass) {
+                        if (item.quality > 0) {
+                            if (!isSulfuras) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = 0;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,208 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class GildedRoseTest {
 
+    private static final String STANDARD_ITEM_NAME = "foo";
+    private static final String AGED_BRIE_ITEM_NAME = "Aged Brie";
+    private static final String SULFURAS_ITEM_NAME = "Sulfuras, Hand of Ragnaros";
+    private static final String BACKSTAGE_PASS_ITEM_NAME = "Backstage passes to a TAFKAL80ETC concert";
+
+    private static final int POSITIVE_DAYS_SELL_IN = 1;
+    private static final int ZERO_DAYS_SELL_IN = 0;
+    private static final int NEGATIVE_DAYS_SELL_IN = -1;
+    private static final int GREATER_THAN_TEN_DAYS_SELL_IN = 11;
+    private static final int TEN_DAYS_SELL_IN = 10;
+    private static final int FIVE_DAYS_SELL_IN = 5;
+
+    private static final int MAXIMUM_QUALITY = 50;
+    private static final int POSITIVE_QUALITY = 5;
+    private static final int ZERO_QUALITY = 0;
+
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void constructorTakesInItemsAndSetsAsProperty() {
+        Item firstItem = new Item(STANDARD_ITEM_NAME, 0, 0);
+        Item secondItem = new Item(STANDARD_ITEM_NAME, 2, 2);
+        Item[] items = new Item[]{firstItem, secondItem};
+
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        assertThat(app.items, is(items));
     }
 
+    @Test
+    public void whenUpdateQualityLowersSellInByOne() {
+        Item[] items = new Item[]{new Item(STANDARD_ITEM_NAME, POSITIVE_DAYS_SELL_IN, 0)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        int expectedSellIn = POSITIVE_DAYS_SELL_IN - 1;
+        assertThat(app.items[0].sellIn, is(expectedSellIn));
+    }
+
+    @Test
+    public void givenNonzeroSellInWhenUpdateQualityThenLowersQualityByOne() {
+        Item[] items = new Item[]{new Item(STANDARD_ITEM_NAME, POSITIVE_DAYS_SELL_IN, POSITIVE_QUALITY)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        int expectedQuality = POSITIVE_QUALITY - 1;
+        assertThat(app.items[0].quality, is(expectedQuality));
+    }
+
+    @Test
+    public void givenMultipleItemsWhenUpdateQualityThenUpdatesForAllItems() {
+        Item firstItem = new Item(STANDARD_ITEM_NAME, 1, 1);
+        Item secondItem = new Item(STANDARD_ITEM_NAME, 2, 2);
+        Item[] items = new Item[]{firstItem, secondItem};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].sellIn, is(0));
+        assertThat(app.items[0].quality, is(0));
+        assertThat(app.items[1].sellIn, is(1));
+        assertThat(app.items[1].quality, is(1));
+    }
+
+    @Test
+    public void givenZeroSellInWhenUpdateQualityThenLowersQualityByTwo() {
+        Item[] items = new Item[]{new Item(STANDARD_ITEM_NAME, ZERO_DAYS_SELL_IN, POSITIVE_QUALITY)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        int expectedQuality = POSITIVE_QUALITY - 2;
+        assertThat(app.items[0].quality, is(expectedQuality));
+    }
+
+    @Test
+    public void givenNegativeSellInWhenUpdateQualityThenLowersQualityByTwo() {
+        Item[] items = new Item[]{new Item(STANDARD_ITEM_NAME, NEGATIVE_DAYS_SELL_IN, POSITIVE_QUALITY)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        int expectedQuality = POSITIVE_QUALITY - 2;
+        assertThat(app.items[0].quality, is(expectedQuality));
+    }
+
+    @Test
+    public void givenPositiveSellInAndZeroQualityWhenUpdateQualityThenQualityRemainsZero() {
+        Item[] items = new Item[]{new Item(STANDARD_ITEM_NAME, POSITIVE_DAYS_SELL_IN, ZERO_QUALITY)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality, is(ZERO_QUALITY));
+    }
+
+    @Test
+    public void givenZeroSellInAndZeroQualityWhenUpdateQualityThenQualityRemainsZero() {
+        Item[] items = new Item[]{new Item(STANDARD_ITEM_NAME, ZERO_DAYS_SELL_IN, ZERO_QUALITY)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality, is(ZERO_QUALITY));
+    }
+
+    @Test
+    public void givenNegativeSellInAndZeroQualityWhenUpdateQualityThenQualityRemainsZero() {
+        Item[] items = new Item[]{new Item(STANDARD_ITEM_NAME, NEGATIVE_DAYS_SELL_IN, ZERO_QUALITY)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality, is(ZERO_QUALITY));
+    }
+
+    @Test
+    public void givenAgedBrieUpdateQualityIncreasesInQuality() {
+        Item[] items = new Item[]{new Item(AGED_BRIE_ITEM_NAME, POSITIVE_DAYS_SELL_IN, ZERO_QUALITY)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        int expectedQuality = ZERO_QUALITY + 1;
+        assertThat(app.items[0].quality, is(expectedQuality));
+    }
+
+    @Test
+    public void givenAgedBrieAndQualityOfFiftyWhenUpdateQualityThenQualityRemainsFifty() {
+        Item[] items = new Item[]{new Item(AGED_BRIE_ITEM_NAME, POSITIVE_DAYS_SELL_IN, MAXIMUM_QUALITY)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality, is(MAXIMUM_QUALITY));
+    }
+
+    @Test
+    public void givenAgedBrieAndSellInLessThanOrEqualToZeroWhenUpdateQualityThenQualityRemainsFifty() {
+        Item[] items = new Item[]{new Item(AGED_BRIE_ITEM_NAME, ZERO_DAYS_SELL_IN, POSITIVE_QUALITY)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality, is(POSITIVE_QUALITY + 2));
+    }
+
+    @Test
+    public void givenSulfurasWhenUpdateQualityThenSellInAndQualityRemainsTheSame() {
+        Item[] items = new Item[]{new Item(SULFURAS_ITEM_NAME, POSITIVE_DAYS_SELL_IN, POSITIVE_QUALITY)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].sellIn, is(POSITIVE_DAYS_SELL_IN));
+        assertThat(app.items[0].quality, is(POSITIVE_QUALITY));
+    }
+
+    @Test
+    public void givenBackstagePassesAndGreaterThanTenDaysSellInWhenUpdateQualityThenQualityIncreasesByOne() {
+        Item[] items = new Item[]{new Item(BACKSTAGE_PASS_ITEM_NAME, GREATER_THAN_TEN_DAYS_SELL_IN, POSITIVE_QUALITY)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality, is(POSITIVE_QUALITY + 1));
+    }
+
+    @Test
+    public void givenBackstagePassesAndTenOrLessDaysSellInWhenUpdateQualityThenQualityIncreasesByTwo() {
+        Item[] items = new Item[]{new Item(BACKSTAGE_PASS_ITEM_NAME, TEN_DAYS_SELL_IN, POSITIVE_QUALITY)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality, is(POSITIVE_QUALITY + 2));
+    }
+
+    @Test
+    public void givenBackstagePassesAndFiveOrLessDaysSellInWhenUpdateQualityThenQualityIncreasesByThree() {
+        Item[] items = new Item[]{new Item(BACKSTAGE_PASS_ITEM_NAME, FIVE_DAYS_SELL_IN, POSITIVE_QUALITY)};
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality, is(POSITIVE_QUALITY + 3));
+    }
+
+    @Test
+    public void givenBackstagePassesAndZeroToNegativeDaysSellInWhenUpdateQualityThenQualityIsSetToZero() {
+        Item[] items = new Item[]{new Item(BACKSTAGE_PASS_ITEM_NAME, ZERO_DAYS_SELL_IN, POSITIVE_QUALITY)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertThat(app.items[0].quality, is(ZERO_QUALITY));
+    }
 }
